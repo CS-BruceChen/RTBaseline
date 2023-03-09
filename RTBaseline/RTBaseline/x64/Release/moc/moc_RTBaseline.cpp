91c5a821fbf76aa3f65a0261080153ba7e92a91f
BruceChen <sharkmansky@163.com>
2023-03-09 21:36:48 +0800

    generate the first release version

--- RTBaseline/RTBaseline/x64/Release/moc/moc_RTBaseline.cpp
+++ RTBaseline/RTBaseline/x64/Release/moc/moc_RTBaseline.cpp
@@ -8,6 +8,7 @@
 
 #include <memory>
 #include "../../../RTBaseline.h"
+#include <QtGui/qtextcursor.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'RTBaseline.h' doesn't include <QObject>."
@@ -26,16 +27,22 @@ QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 namespace {
 struct qt_meta_stringdata_RTBaseline_t {
-    uint offsetsAndSizes[2];
+    uint offsetsAndSizes[6];
     char stringdata0[11];
+    char stringdata1[34];
+    char stringdata2[1];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_RTBaseline_t::offsetsAndSizes) + ofs), len 
 Q_CONSTINIT static const qt_meta_stringdata_RTBaseline_t qt_meta_stringdata_RTBaseline = {
     {
-        QT_MOC_LITERAL(0, 10)   // "RTBaseline"
+        QT_MOC_LITERAL(0, 10),  // "RTBaseline"
+        QT_MOC_LITERAL(11, 33),  // "on_textEdit_cursorPositionCha..."
+        QT_MOC_LITERAL(45, 0)   // ""
     },
-    "RTBaseline"
+    "RTBaseline",
+    "on_textEdit_cursorPositionChanged",
+    ""
 };
 #undef QT_MOC_LITERAL
 } // unnamed namespace
@@ -46,12 +53,18 @@ Q_CONSTINIT static const uint qt_meta_data_RTBaseline[] = {
       10,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       1,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   20,    2, 0x08,    1 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
 
        0        // eod
 };
@@ -64,16 +77,23 @@ Q_CONSTINIT const QMetaObject RTBaseline::staticMetaObject = { {
     nullptr,
     qt_incomplete_metaTypeArray<qt_meta_stringdata_RTBaseline_t,
         // Q_OBJECT / Q_GADGET
-        QtPrivate::TypeAndForceComplete<RTBaseline, std::true_type>
+        QtPrivate::TypeAndForceComplete<RTBaseline, std::true_type>,
+        // method 'on_textEdit_cursorPositionChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
     >,
     nullptr
 } };
 
 void RTBaseline::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    (void)_o;
-    (void)_id;
-    (void)_c;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<RTBaseline *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_textEdit_cursorPositionChanged(); break;
+        default: ;
+        }
+    }
     (void)_a;
 }
 
@@ -93,6 +113,17 @@ void *RTBaseline::qt_metacast(const char *_clname)
 int RTBaseline::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 1)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 1;
+    }
     return _id;
 }
 QT_WARNING_POP
